4907fdf21a897d7cee029d7f3ff2c315c2d688ef
hamza-khan-16 <hamzakhanbsc16@gmail.com>
2025-02-17 23:55:49 -0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,  
+    allowedHosts: [
+      '9b8f-103-157-153-113.ngrok-free.app',
+      'localhost', 
+      '.ngrok.io' ,
+     'http://192.168.154.4:5173/'
+    ],
+  },
+})
+
